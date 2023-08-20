7097886e6971d2d1afd252710a92916b2cc68dc9
sonb9615 <sonb9615@gmail.com>
2023-08-20 23:08:07 +0900

    Member 테이블 컬럼 추가 role, regDt, uptDt, name

--- server/src/main/java/com/balancegame/server/member/domain/Member.java
+++ server/src/main/java/com/balancegame/server/member/domain/Member.java
@@ -1,8 +1,12 @@
 package com.balancegame.server.member.domain;
 
+import com.balancegame.server.common.domain.BaseDateEntity;
 import com.balancegame.server.security.enums.AuthProvider;
+import com.balancegame.server.security.enums.Role;
+import com.balancegame.server.security.oAuth.info.OAuth2UserInfo;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,20 +14,49 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseDateEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
+    private String name;
     private String email;
     private String password;
-    private String type;
 
     @Enumerated(EnumType.STRING)
     private AuthProvider authProvider;
 
-//    @Enumerated(EnumType.STRING)
-//    private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String oAuth2Id;
+
+    @Builder
+    public Member(Long memberId, String name, String email, String password, AuthProvider authProvider, Role role, String oAuth2Id) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.authProvider = authProvider;
+        this.role = role;
+        this.oAuth2Id = oAuth2Id;
+    }
+
+    public void updateOAuthInfo(OAuth2UserInfo oAuth2UserInfo){
+        this.oAuth2Id = oAuth2UserInfo.getOAuth2Id();
+        this.name = oAuth2UserInfo.getName();
+    }
+
+    public Member registerMember(AuthProvider authProvider, OAuth2UserInfo oAuth2UserInfo){
+        return Member.builder()
+                .name(oAuth2UserInfo.getName())
+                .email(oAuth2UserInfo.getEmail())
+                .authProvider(authProvider)
+                .role(Role.ROLE_USER)
+                .oAuth2Id(oAuth2UserInfo.getOAuth2Id())
+                .build();
+    }
+
 }
 
